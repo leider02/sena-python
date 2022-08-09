99d5edcc5841becfedb8217a87d1e4056fe51ee9
leiderracero <leiderracerohoyos@gmail.com>
2022-08-08 23:40:14 -0500

    dias trabajados

---
+++ trimestre1/estructuras-secuenciales/dias-trabajados.py
@@ -0,0 +1,8 @@
+# pedir al usuario dias trabajados y valor por dias
+dias_trabajados = float(input("ingresar dias trabajados: ")) 
+valor_dias = float(input("ingresar valor dias: ")) 
+# valor total a pagar 
+valor_pagar = dias_trabajados * valor_dias
+
+# imprimir 
+print("el valor de pago es:", valor_pagar)
